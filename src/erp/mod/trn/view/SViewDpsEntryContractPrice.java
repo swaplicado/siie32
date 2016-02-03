be220f7f6c619858b618787f12152360bd064bea
Software Aplicado <contacto@swaplicado.com.mx>
2016-02-03 17:29:50 -0600

    Release SIIE 3.2 035.04.
    - Fix to views of contracts to be linked and linked by month.

--- src/erp/mod/trn/view/SViewDpsEntryContractPrice.java
+++ src/erp/mod/trn/view/SViewDpsEntryContractPrice.java
@@ -12,6 +12,7 @@ import erp.lib.SLibConstants;
 import erp.lib.SLibUtilities;
 import erp.mcfg.data.SDataParamsErp;
 import erp.mod.SModConsts;
+import erp.mod.SModSysConsts;
 import erp.mod.trn.form.SDialogContractPriceCardex;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -148,6 +149,8 @@ public class SViewDpsEntryContractPrice extends SGridPaneView implements ActionL
 
     @Override
     public void prepareSqlQuery() {
+        int idCategory = isViewForCategoryPur() ? SModSysConsts.BPSS_CT_BP_SUP : SModSysConsts.BPSS_CT_BP_CUS;
+        int[] keyDpsType = isViewForCategoryPur() ? SModSysConsts.TRNU_TP_DPS_PUR_CON : SModSysConsts.TRNU_TP_DPS_SAL_CON;
         String sql = "";
         Object filter = null;
         
@@ -155,40 +158,78 @@ public class SViewDpsEntryContractPrice extends SGridPaneView implements ActionL
 
         if (mnGridMode != SModConsts.VIEW_ST_PEND) {
             filter = (SGuiDate) moFiltersMap.get(SGridConsts.FILTER_DATE_PERIOD).getValue();
-            sql += (sql.isEmpty() ? "AND " : "") + SGridUtils.getSqlFilterDate("d.dt", (SGuiDate) filter);
+            sql += (sql.isEmpty() ? "" : "AND ") + SGridUtils.getSqlFilterDate("d.dt", (SGuiDate) filter) + " ";
         }
         else {
             filter = (SGuiDate) moFiltersMap.get(SGridConsts.FILTER_DATE).getValue();
             if (filter != null) {
-                sql += (sql.isEmpty() ? "AND " : "") + "d.dt <= '" + SLibUtils.DbmsDateFormatDate.format((SGuiDate) filter) + "' ";
+                sql += (sql.isEmpty() ? "" : "AND ") + "d.dt <= '" + SLibUtils.DbmsDateFormatDate.format((SGuiDate) filter) + "' ";
             }
         }
         
-        
-        msSql = "SELECT de.id_year AS " + SDbConsts.FIELD_ID + "1, "
+        msSql = "SELECT "
+                + "de.id_year AS " + SDbConsts.FIELD_ID + "1, "
                 + "de.id_doc AS " + SDbConsts.FIELD_ID + "2, "
                 + "de.id_ety AS " + SDbConsts.FIELD_ID + "3, "
                 + "'' AS " + SDbConsts.FIELD_CODE + ", "
                 + "'' AS " + SDbConsts.FIELD_NAME + ", "
-                + "(SELECT cob.code FROM erp.bpsu_bpb AS cob WHERE d.fid_cob = cob.id_bpb) AS f_cob_code, "
-                + "d.b_link, d.dt, d.num_ref, CONCAT(d.num_ser, IF(length(d.num_ser) = 0, '', '-'), d.num) AS f_num, dt.code, "
-                + "COALESCE(SUM(dps_sup.orig_qty), 0) AS f_orig_qty_supply, de.orig_qty AS f_orig_qty_prc, "
-                + "it.item, de.concept_key, u.symbol, bp.bp, bpc.bp_key "
+                + "CONCAT(d.num_ser, IF(length(d.num_ser) = 0, '', '-'), d.num) AS f_num, "
+                + "d.dt, d.num_ref, d.b_link, d.fid_cob, d.fid_bpb, d.fid_bp_r, "
+                + "dt.code, dt.tp_dps, cob.code, cob.bpb, b.bp, bc.bp_key, bb.bpb, "
+                + "i.item, de.concept_key, u.symbol, uo.symbol, "
+                + "de.orig_qty AS f_orig_qty, "
+                + "COALESCE(SUM(ds.orig_qty), 0) AS f_orig_qty_supply "
                 + "FROM " + SModConsts.TablesMap.get(SModConsts.TRN_DPS) + " AS d "
-                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.TRN_DPS_ETY) + " AS de ON de.id_year = d.id_year AND de.id_doc = d.id_doc "
-                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.TRNU_TP_DPS) + " AS dt ON d.fid_ct_dps = dt.id_ct_dps AND d.fid_cl_dps = dt.id_cl_dps AND d.fid_tp_dps = dt.id_tp_dps "
-                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.BPSU_BP) + " AS bp ON bp.id_bp = d.fid_bp_r "
-                + "INNER JOIN erp.bpsu_bp_ct AS bpc ON bp.id_bp = bpc.id_bp AND bpc.id_ct_bp = "
-                + (isViewForCategoryPur() ? SDataConstantsSys.BPSS_CT_BP_SUP : SDataConstantsSys.BPSS_CT_BP_CUS) + " "
-                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.ITMU_ITEM) + " AS it ON de.fid_item = it.id_item "
-                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.ITMU_UNIT) + " AS u ON u.id_unit = de.fid_unit "
-                + "LEFT OUTER JOIN " + SModConsts.TablesMap.get(SModConsts.TRN_DPS_DPS_SUPPLY) + " AS dps_sup ON dps_sup.id_src_year = de.id_year AND dps_sup.id_src_doc = de.id_doc AND dps_sup.id_src_ety = de.id_ety "
+                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.TRN_DPS_ETY) + " AS de ON "
+                + "d.id_year = de.id_year AND d.id_doc = de.id_doc "
+                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.TRNU_TP_DPS) + " AS dt ON "
+                + "d.fid_ct_dps = dt.id_ct_dps AND d.fid_cl_dps = dt.id_cl_dps AND d.fid_tp_dps = dt.id_tp_dps "
+                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.BPSU_BPB) + " AS cob ON "
+                + "d.fid_cob = cob.id_bpb "
+                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.BPSU_BP) + " AS b ON "
+                + "d.fid_bp_r = b.id_bp "
+                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.BPSU_BP_CT) + " AS bc ON "
+                + "d.fid_bp_r = bc.id_bp AND bc.id_ct_bp = " + idCategory + " "
+                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.BPSU_BPB) + " AS bb ON "
+                + "d.fid_bpb = bb.id_bpb "
+                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.ITMU_ITEM) + " AS i ON "
+                + "de.fid_item = i.id_item "
+                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.ITMU_UNIT) + " AS u ON "
+                + "de.fid_unit = u.id_unit "
+                + "INNER JOIN " + SModConsts.TablesMap.get(SModConsts.ITMU_UNIT) + " AS uo ON "
+                + "de.fid_orig_unit = uo.id_unit "
+                + "LEFT OUTER JOIN " + SModConsts.TablesMap.get(SModConsts.TRN_DPS_DPS_SUPPLY) + " AS ds ON "
+                + "de.id_year = ds.id_src_year AND de.id_doc = ds.id_src_doc AND de.id_ety = ds.id_src_ety "
                 + "WHERE EXISTS (SELECT * FROM trn_dps_ety_prc AS dep WHERE dep.id_year = de.id_year AND dep.id_doc = de.id_doc AND dep.id_ety = de.id_ety) AND "
-                + (isViewForCategoryPur() ? "d.fid_ct_dps = " + SDataConstantsSys.TRNU_TP_DPS_PUR_CON[0] + " AND d.fid_cl_dps = " + SDataConstantsSys.TRNU_TP_DPS_PUR_CON[1] + " AND d.fid_tp_dps = " + SDataConstantsSys.TRNU_TP_DPS_PUR_CON[2] : 
-                "d.fid_ct_dps = " + SDataConstantsSys.TRNU_TP_DPS_SAL_CON[0] + " AND d.fid_cl_dps = " + SDataConstantsSys.TRNU_TP_DPS_SAL_CON[1] + " AND d.fid_tp_dps = " + SDataConstantsSys.TRNU_TP_DPS_SAL_CON[2]) + " " + sql
-                + "GROUP BY de.id_year, de.id_doc, de.id_ety "
-                + (mnGridMode == SModConsts.VIEW_ST_PEND ? "HAVING f_orig_qty_prc < f_orig_qty_supply AND d.b_link = 0 " : "HAVING f_orig_qty_prc >= f_orig_qty_supply OR d.b_link = 1 ")
-                + "ORDER BY de.id_year, de.id_doc, de.id_ety; ";
+                + "d.fid_ct_dps = " + keyDpsType[0] + " AND d.fid_cl_dps = " + keyDpsType[1] + " AND d.fid_tp_dps = " + keyDpsType[2] + " " + (sql.isEmpty() ? "" : "AND " + sql)
+                + "GROUP BY de.id_year, de.id_doc, de.id_ety, d.num_ser, d.num, "
+                + "d.dt, d.num_ref, d.b_link, d.fid_cob, d.fid_bpb, d.fid_bp_r, "
+                + "dt.code, dt.tp_dps, cob.code, cob.bpb, b.bp, bc.bp_key, bb.bpb, "
+                + "i.item, de.concept_key, u.symbol, uo.symbol, "
+                + "de.orig_qty "
+                + (mnGridMode == SModConsts.VIEW_ST_PEND ? "HAVING f_orig_qty_supply < f_orig_qty AND d.b_link = 0 " : "HAVING f_orig_qty_supply >= f_orig_qty OR d.b_link = 1 ") + " ";
+        
+            msSql += "ORDER BY dt.code, d.num_ser, CAST(d.num AS UNSIGNED INTEGER), d.num, d.dt, de.id_year, de.id_doc, ";
+
+            if (isViewForCategoryPur()) {
+                msSql += ((SDataParamsErp) miClient.getSession().getConfigSystem()).getFkSortingSupplierTypeId() == SDataConstantsSys.CFGS_TP_SORT_KEY_NAME ?
+                    "bc.bp_key, b.bp, " : "b.bp, bc.bp_key, ";
+            }
+            else {
+                msSql += ((SDataParamsErp) miClient.getSession().getConfigSystem()).getFkSortingCustomerTypeId() == SDataConstantsSys.CFGS_TP_SORT_KEY_NAME ?
+                    "bc.bp_key, b.bp, " : "b.bp, bc.bp_key, ";
+            }
+
+            msSql += "d.fid_bp_r, bb.bpb, d.fid_bpb, ";
+
+            if (((SDataParamsErp) miClient.getSession().getConfigSystem()).getFkSortingItemTypeId() == SDataConstantsSys.CFGS_TP_SORT_KEY_NAME) {
+                msSql += "i.item_key, i.item, ";
+            }
+            else {
+                msSql += "i.item, i.item_key, ";
+            }
+
+            msSql += "de.fid_item, uo.symbol, de.fid_orig_unit ";
     }
 
     @Override
@@ -200,37 +241,52 @@ public class SViewDpsEntryContractPrice extends SGridPaneView implements ActionL
         gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_REG_NUM, "f_num", "Folio doc"));
         gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT, "num_ref", "Referencia doc"));
         gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_DATE, "d.dt", "Fecha doc"));
-        gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_CO, "f_cob_code", "Sucursal empresa"));
+        gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_CO, "cob.code", "Sucursal empresa"));
+        
         if (isViewForCategoryPur()) {
             if (((SDataParamsErp) miClient.getSession().getConfigSystem()).getFkSortingSupplierTypeId() == SDataConstantsSys.CFGS_TP_SORT_KEY_NAME) {
-                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_BPR, "bpc.bp_key", "Clave proveedor"));
-                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_BPR_L, "bp.bp", "Proveedor"));
+                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_BPR, "bc.bp_key", "Clave proveedor"));
+                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_BPR_L, "b.bp", "Proveedor"));
             }
             else {
-                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_BPR_L, "bp.bp", "Proveedor"));
-                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_BPR, "bpc.bp_key", "Clave proveedor"));
+                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_BPR_L, "b.bp", "Proveedor"));
+                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_BPR, "bc.bp_key", "Clave proveedor"));
             }
         }
         else {
             if (((SDataParamsErp) miClient.getSession().getConfigSystem()).getFkSortingCustomerTypeId() == SDataConstantsSys.CFGS_TP_SORT_KEY_NAME) {
-                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_BPR, "bpc.bp_key", "Clave cliente"));
-                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_BPR_L, "bp.bp", "Cliente"));
+                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_BPR, "bc.bp_key", "Clave cliente"));
+                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_BPR_L, "b.bp", "Cliente"));
             }
             else {
-                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_BPR_L, "bp.bp", "Cliente"));
-                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_BPR, "bpc.bp_key", "Clave cliente"));
+                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_BPR_L, "b.bp", "Cliente"));
+                gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_BPR, "bc.bp_key", "Clave cliente"));
             }
         }
-        gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_ITM, "de.concept_key", "Clave ítem"));
-        gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_ITM_L, "it.item", "Ítem"));
-        gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_DEC_4D, "f_orig_qty_prc", "Cantidad"));
+        
+        if (((SDataParamsErp) miClient.getSession().getConfigSystem()).getFkSortingItemTypeId() == SDataConstantsSys.CFGS_TP_SORT_KEY_NAME) {
+            gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_ITM, "de.concept_key", "Clave ítem"));
+            gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_ITM_L, "i.item", "Ítem"));
+        }
+        else {
+            gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_NAME_ITM_L, "i.item", "Ítem"));
+            gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_ITM, "de.concept_key", "Clave ítem"));
+        }
+        
+        gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_DEC_4D, "f_orig_qty", "Cantidad"));
         gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_DEC_4D, "f_orig_qty_supply", "Cant procesada"));
+        column = new SGridColumnView(SGridConsts.COL_TYPE_DEC_PER_2D, "", "Avance %");
+        column.getRpnArguments().add(new SLibRpnArgument("f_orig_qty_supply", SLibRpnArgumentType.OPERAND));
+        column.getRpnArguments().add(new SLibRpnArgument("f_orig_qty", SLibRpnArgumentType.OPERAND));
+        column.getRpnArguments().add(new SLibRpnArgument(SLibRpnOperator.DIVISION, SLibRpnArgumentType.OPERATOR));
+        gridColumnsViews.add(column);
         column = new SGridColumnView(SGridConsts.COL_TYPE_DEC_4D, "", "Cant pendiente");
-        column.getRpnArguments().add(new SLibRpnArgument("f_orig_qty_prc", SLibRpnArgumentType.OPERAND));
+        column.getRpnArguments().add(new SLibRpnArgument("f_orig_qty", SLibRpnArgumentType.OPERAND));
         column.getRpnArguments().add(new SLibRpnArgument("f_orig_qty_supply", SLibRpnArgumentType.OPERAND));
         column.getRpnArguments().add(new SLibRpnArgument(SLibRpnOperator.SUBTRACTION, SLibRpnArgumentType.OPERATOR));
         gridColumnsViews.add(column);
         gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_TEXT_CODE_UNT, "u.symbol", "Unidad"));
+        gridColumnsViews.add(new SGridColumnView(SGridConsts.COL_TYPE_BOOL_S, "d.b_link", "Cerrado"));
 
         return gridColumnsViews;
     }
