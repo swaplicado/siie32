e1fd474f925eaeaa1304317040897f22f4d4f81e
Software Aplicado <contacto@swaplicado.com.mx>
2016-04-18 17:17:20 -0500

    - Minor changes: standardization of source code syntax improvement.

--- src/erp/mcfg/form/SFormCurrency.java
+++ src/erp/mcfg/form/SFormCurrency.java
@@ -329,7 +329,7 @@ public class SFormCurrency extends javax.swing.JDialog implements erp.lib.form.S
         SFormValidation validation = new SFormValidation();
 
         for (int i = 0; i < mvFields.size(); i++) {
-            if (!((erp.lib.form.SFormField) mvFields.get(i)).validateField()){
+            if (!((erp.lib.form.SFormField) mvFields.get(i)).validateField()) {
                 validation.setIsError(true);
                 validation.setComponent(((erp.lib.form.SFormField) mvFields.get(i)).getComponent());
                 break;
