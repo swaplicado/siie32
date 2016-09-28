7b879e6b4917d5664506695a5a24250a4023631f
Software Aplicado <Software Aplicado@192.168.1.30>
2016-09-28 17:49:03 -0500

    Fix of data lock recovery.

--- src/erp/server/SServiceDataLocks.java
+++ src/erp/server/SServiceDataLocks.java
@@ -5,11 +5,17 @@
 
 package erp.server;
 
+import erp.data.SDataConstants;
+import erp.lib.SLibUtilities;
+import erp.mod.SModConsts;
 import java.io.Serializable;
+import java.sql.ResultSet;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Vector;
 import sa.lib.SLibConsts;
 import sa.lib.SLibUtils;
+import sa.lib.db.SDbConsts;
 import sa.lib.srv.SSrvConsts;
 import sa.lib.srv.SSrvLock;
 
@@ -67,6 +73,84 @@ public class SServiceDataLocks implements Serializable {
         return lockFound;
     }
 
+    /** Obtains last update timestamp of provided data registry.
+     * 
+     * @param sessionId Client user session.
+     * @param typeRegistry Constants defined in erp.data.SDataConstants.
+     * @param pk Primary key of data registry.
+     * @return Last timestamp of data registry when found, otherwise <code>null</code>.
+     */
+    private Date getLastUpdateTimestamp(final int sessionId, final int registryType, final Object pk) {
+        int i = 0;
+        Date lastTs = null;
+        String fieldNameTs = "";
+        String tableName = "";
+        String sql = "";
+        HashMap<String, String> primaryKeyNames = new HashMap<>();
+        ResultSet resultSet = null;
+         
+        if (registryType <= SDataConstants.MAX_ID) {
+            // Former framework table and column names:
+            
+            tableName = SDataConstants.TablesMap.get(registryType);
+            fieldNameTs = "ts_edit";
+        }
+        else {
+            // Current framework table and column names:
+            
+            tableName = SModConsts.TablesMap.get(registryType);
+            fieldNameTs = "ts_usr_upd";
+        }
+        
+        try {
+            // Retrieve primary key columns:
+            
+            sql = "SHOW COLUMNS FROM " + tableName + " WHERE `Key` = 'PRI' ";
+            resultSet = moServer.getServiceSessions().getSession(sessionId).getStatement().executeQuery(sql);
+                
+            while (resultSet.next()) {
+                primaryKeyNames.put(resultSet.getString("Field"), resultSet.getString("Type"));
+            }
+            
+            // Validate size of provided key vs. size of retrieved primary key columns:
+            
+            if (((Object[]) pk).length != primaryKeyNames.size()) {
+                throw new Exception(SDbConsts.ERR_MSG_REG_NOT_FOUND + "\n" + SSrvConsts.MSG_ERR_LOCK_ON_VERIFY);
+            }
+            
+            // Get last timestamp of desired registry:
+            
+            sql = "SELECT " + fieldNameTs + " FROM " + tableName + " WHERE ";
+
+            for (HashMap.Entry<String, String> entry : primaryKeyNames.entrySet()) {
+                sql += entry.getKey() + " = ";
+                
+                if (entry.getValue().contains("char")) {
+                    sql += "'" + ((String) ((Object[]) pk)[i++]) + "' ";
+                }
+                else if (entry.getValue().contains("date")) {
+                    sql += "'" + SLibUtils.DbmsDateFormatDate.format((Date) ((Object[]) pk)[i++]) + "' ";
+                }
+                else {
+                    sql += ((Integer) ((Object[]) pk)[i++]) + " ";
+                }
+            }
+
+            resultSet = moServer.getServiceSessions().getSession(sessionId).getStatement().executeQuery(sql);
+
+            if (!resultSet.next()) {
+                throw new Exception(SDbConsts.ERR_MSG_REG_NOT_FOUND);
+            }
+            else {
+                lastTs = resultSet.getTimestamp(fieldNameTs);
+            }
+        }
+        catch (java.lang.Exception e) {
+            SLibUtilities.printOutException(this, e);
+        } 
+        
+        return lastTs;
+    }
     /*
      * Public methods:
      */
@@ -82,7 +166,7 @@ public class SServiceDataLocks implements Serializable {
 
         while (i < mvLocks.size()) {
             if (!isLockAlive(mvLocks.get(i))) {
-                mvLocks.remove(i);
+              mvLocks.remove(i);
             }
             else {
                 i++;    // evaluate next lock
@@ -175,20 +259,30 @@ public class SServiceDataLocks implements Serializable {
     }
 
     public synchronized SSrvLock recoverLock(final int sessionId, final int companyId, final int registryType, final Object key, final long timeout, final Date timestamp_n) {
-        SSrvLock lock = getLock(companyId, registryType, key);
+        Date lastTs = null;
+        SSrvLock lock = new SSrvLock(sessionId, companyId, registryType, key, timeout, moServer.getSystemDatetime());
 
         moServer.renderMessageLn(msService + "Recovering lock for session [" + sessionId + "] (company=" + companyId + ", type=" + registryType + ")...");
 
-        if (lock != null) {
-            lock = lock.clone();
+        if (getLock(companyId, registryType, key) != null) {
+            // Data registry is already locked:
+            
             lock.setLockStatus(SSrvConsts.LOCK_RECOVER_DENIED);
         }
         else {
-            lock = new SSrvLock(sessionId, companyId, registryType, key, timeout, moServer.getSystemDatetime());
-            lock.setLockId(++mlCurrentLockId);
-            lock.setLockStatus(SSrvConsts.LOCK_RECOVERED);
-            lock.setLockUser(moServer.getServiceSessions().getSession(lock.getSessionId()).getUserName());
-            mvLocks.add(lock);
+            // Lock is not longer active, check if it can be recovered:
+            
+            lastTs = getLastUpdateTimestamp(sessionId, registryType, key);
+            
+            if (lastTs == null || lastTs.after(timestamp_n)) {
+                lock.setLockStatus(SSrvConsts.LOCK_RECOVER_DENIED);
+            }
+            else {
+                lock.setLockId(++mlCurrentLockId);
+                lock.setLockStatus(SSrvConsts.LOCK_RECOVERED);
+                lock.setLockUser(moServer.getServiceSessions().getSession(lock.getSessionId()).getUserName());
+                mvLocks.add(lock);
+            }
         }
 
         return lock;
