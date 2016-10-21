818b174510415ad4797f5b35c4d5187e77d9f1fa
Software Aplicado <Software Aplicado@192.168.1.30>
2016-10-21 11:24:44 -0500

    Add new Class SMoney

---
+++ src/erp/mod/fin/db/SMoney.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erp.mod.fin.db;
+
+import sa.lib.SLibUtils;
+/**
+ *
+ * @author Software Aplicado
+ */
+public class SMoney {
+    private int 	mnRoundingDecimals;
+    private int 	mnCurrencyOriginalId;
+    private int 	mnCurrencyLocalId;
+    private double	mdAmountOriginal;
+    private double	mdExchangeRate;
+
+    private static final int ROUND_DEC = 2;
+
+    public SMoney( double amountOriginal, int currencyOriginalId) {
+        mnRoundingDecimals = ROUND_DEC;
+        mnCurrencyOriginalId = currencyOriginalId;
+        mnCurrencyLocalId = currencyOriginalId;
+        mdAmountOriginal = amountOriginal;
+        mdExchangeRate = (mnCurrencyOriginalId == mnCurrencyLocalId ? 1 : 0);
+    }
+    
+    public SMoney( double amountOriginal, int currencyOriginalId, double exchangeRate, int currencyLocalId) {
+        mnRoundingDecimals = ROUND_DEC;
+        mnCurrencyOriginalId = currencyOriginalId;
+        mnCurrencyLocalId = currencyLocalId;
+        mdAmountOriginal = amountOriginal;
+        mdExchangeRate = (mnCurrencyOriginalId == mnCurrencyLocalId ? 1 : exchangeRate);
+    }
+    
+    public int getRoundingDecimals() { return mnRoundingDecimals; }
+    public int getCurrencyOriginalId() { return mnCurrencyOriginalId; }
+    public int getCurrencyLocalId() { return mnCurrencyLocalId; }
+    public double getAmountOriginal() { return mdAmountOriginal; }
+    public double getExchangeRate() { return mdExchangeRate; }
+
+    public double getAmountLocal () {
+        return SLibUtils.round(mdAmountOriginal * mdExchangeRate, SLibUtils.getDecimalFormatAmount().getMaximumFractionDigits());
+    }
+    
+    public void setExchangeRate(double d) { mdExchangeRate = d; }
+    public void setRoundingDecimals(int i) { mnRoundingDecimals = i; }
+    public void setAmountOriginal(double d) { mdAmountOriginal = d; }
+    
+    public void addAmountOriginal(double d){
+        mdAmountOriginal = SLibUtils.round(mdAmountOriginal + d, SLibUtils.getDecimalFormatAmount().getMaximumFractionDigits());
+    }
+    
+    public void subtractAmountOginal(double d){
+        mdAmountOriginal = SLibUtils.round(mdAmountOriginal - d, SLibUtils.getDecimalFormatAmount().getMaximumFractionDigits());
+    }
+
+}
