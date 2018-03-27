fdf8fc2a8b6d62d95637d3fd0dfbf025603958a2
Software Aplicado <Software Aplicado@SWAP01>
2018-03-27 16:51:04 -0600

    Release 3.2 113.03: Implementation of required customs unit for CFDI defined in items.

--- src/erp/SClient.java
+++ src/erp/SClient.java
@@ -106,7 +106,7 @@ import sa.lib.xml.SXmlUtils;
 public class SClient extends JFrame implements ActionListener, SClientInterface, SGuiClient {
 
     public static final String APP_NAME = "SIIE 3.2";
-    public static final String APP_RELEASE = "3.2 113.02";
+    public static final String APP_RELEASE = "3.2 113.03";
     public static final String APP_COPYRIGHT = "2007-2018";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
