469e5f928cdf4b1c802a5f118079a719c1327799
Brianda <Brianda@Swap-03>
2018-08-21 12:32:56 -0500

    Integration of Basex for storing XML files.

--- src/erp/mtrn/data/SDataCfd.java
+++ src/erp/mtrn/data/SDataCfd.java
@@ -6,6 +6,8 @@
 package erp.mtrn.data;
 
 import cfd.DCfdUtils;
+import erp.SBaseXClient;
+import erp.SBaseXUtils;
 import erp.cfd.SCfdConsts;
 import erp.data.SDataConstantsSys;
 import erp.lib.SLibConstants;
@@ -16,6 +18,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
@@ -59,6 +62,7 @@ public class SDataCfd extends erp.lib.data.SDataRegistry implements java.io.Seri
     protected java.lang.String msCertNumber;
     protected java.lang.String msStringSigned;
     protected java.lang.String msSignature;
+    protected int mnNodeId_n;
     protected java.lang.String msDocXml;
     protected java.lang.String msDocXmlName;
     protected java.lang.String msDocXmlRfcEmi;
@@ -187,6 +191,7 @@ public class SDataCfd extends erp.lib.data.SDataRegistry implements java.io.Seri
     public void setCertNumber(java.lang.String s) { msCertNumber = s; }
     public void setStringSigned(java.lang.String s) { msStringSigned = s; }
     public void setSignature(java.lang.String s) { msSignature = s; }
+    public void setNodeId_n(int n) { mnNodeId_n = n; }
     public void setDocXml(java.lang.String s) { msDocXml = s; }
     public void setDocXmlName(java.lang.String s) { msDocXmlName = s; }
     public void setDocXmlRfcEmi(java.lang.String s) { msDocXmlRfcEmi = s; }
@@ -243,6 +248,7 @@ public class SDataCfd extends erp.lib.data.SDataRegistry implements java.io.Seri
     public java.lang.String getCertNumber() { return msCertNumber; }
     public java.lang.String getStringSigned() { return msStringSigned; }
     public java.lang.String getSignature() { return msSignature; }
+    public int getNodeId_n() { return mnNodeId_n; }
     public java.lang.String getDocXml() { return msDocXml; }
     public java.lang.String getDocXmlName() { return msDocXmlName; }
     public java.lang.String getDocXmlRfcEmi() { return msDocXmlRfcEmi; }
@@ -404,6 +410,7 @@ public class SDataCfd extends erp.lib.data.SDataRegistry implements java.io.Seri
         msCertNumber = "";
         msStringSigned = "";
         msSignature = "";
+        mnNodeId_n = -1;    // means null
         msDocXml = "";
         msDocXmlName = "";
         msDocXmlRfcEmi = "";
@@ -479,7 +486,30 @@ public class SDataCfd extends erp.lib.data.SDataRegistry implements java.io.Seri
                 msCertNumber = resultSet.getString("cert_num");
                 msStringSigned = resultSet.getString("str_signed");
                 msSignature = resultSet.getString("signature");
+                mnNodeId_n = resultSet.getInt("node_id_n");
+                if (resultSet.wasNull()) {
+                    mnNodeId_n = -1;    // means null
+                }
+                
+                /*                
+                    msDocXml = db:open-id("TreeBD", resultSet.getString("doc_xml"))                
+                */
+                String mysqlDatabaseURL = statement.getConnection().getMetaData().getURL();
+            
+                String databaseHost = SBaseXUtils.getDbHostFromUrl(mysqlDatabaseURL);
+                String databaseName = SBaseXUtils.getDbNameFromUrl(mysqlDatabaseURL);
+            
+                // Company BaseX database connection
+                SBaseXClient baseXSession = SBaseXUtils.getBaseXSessionInstance(databaseHost, 1984, "admin", "admin");
+                
+                String getDocumentByNodeID = "db:open-id(\""+ databaseName +"\"," + resultSet.getString("node_id_n") + " )";                
+                String xmlDocument  = SBaseXUtils.executeBaseXQuery(baseXSession, getDocumentByNodeID).get(0);
+                
+                msDocXml = xmlDocument;
+                
                 msDocXml = resultSet.getString("doc_xml");
+                
+                
                 msDocXmlName = resultSet.getString("doc_xml_name");
                 msDocXmlRfcEmi = resultSet.getString("xml_rfc_emi");
                 msDocXmlRfcRec = resultSet.getString("xml_rfc_rec");
@@ -577,13 +607,13 @@ public class SDataCfd extends erp.lib.data.SDataRegistry implements java.io.Seri
                 }
 
                 sql = "INSERT INTO trn_cfd (id_cfd, ser, num, " +
-                        "ts, cert_num, str_signed, signature, doc_xml, doc_xml_name, xml_rfc_emi, xml_rfc_rec, xml_tot, xml_mon, " +
+                        "ts, cert_num, str_signed, signature, node_id_n, doc_xml, doc_xml_name, xml_rfc_emi, xml_rfc_rec, xml_tot, xml_mon, " +
                         "xml_tc, xml_sign_n, uuid, qrc_n, ack_can_xml, ack_can_pdf_n, ack_dvy, msg_dvy, b_prc_ws, b_prc_sto_xml, " +
                         "b_prc_sto_pdf, b_con, fid_tp_cfd, fid_tp_xml, fid_st_xml, fid_tp_xml_dvy, fid_st_xml_dvy, fid_cob_n, fid_dps_year_n, fid_dps_doc_n, " +
                         "fid_rec_year_n, fid_rec_per_n, fid_rec_bkc_n, fid_rec_tp_rec_n, fid_rec_num_n, fid_rec_ety_n, fid_pay_pay_n, fid_pay_emp_n, fid_pay_bpr_n, fid_pay_rcp_pay_n, fid_pay_rcp_emp_n, " +
                         "fid_pay_rcp_iss_n, fid_usr_prc, fid_usr_dvy, ts_prc, ts_dvy) " +
                         "VALUES (" + mnPkCfdId + ", ?, ?, " +
-                        "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, " +
+                        "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, " +
                         "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, " +
                         "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, " +
                         "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, " +
@@ -592,7 +622,7 @@ public class SDataCfd extends erp.lib.data.SDataRegistry implements java.io.Seri
             else {
                 bIsUpd = true;
                 
-                sql = "UPDATE trn_cfd SET ser = ?, num = ?, ts = ?, cert_num = ?, str_signed = ?, signature = ?, " +
+                sql = "UPDATE trn_cfd SET ser = ?, num = ?, ts = ?, cert_num = ?, str_signed = ?, signature = ?, node_id_n = ?, " +
                         "doc_xml = ?, doc_xml_name = ?, xml_rfc_emi = ?, xml_rfc_rec = ?, xml_tot = ?, xml_mon = ?, xml_tc = ?, xml_sign_n = ?, " +
                         "uuid = ?, qrc_n = ?, ack_can_xml = ?, ack_dvy = ?, msg_dvy = ?, b_con = ?, " +
                         "fid_tp_cfd = ?, fid_tp_xml = ?, fid_st_xml = ?, fid_tp_xml_dvy = ?, fid_st_xml_dvy = ?, fid_cob_n = ?, " +
@@ -611,6 +641,39 @@ public class SDataCfd extends erp.lib.data.SDataRegistry implements java.io.Seri
             preparedStatement.setString(index++, msCertNumber);
             preparedStatement.setString(index++, msStringSigned);
             preparedStatement.setString(index++, msSignature);
+            if (mnNodeId_n == -1) {
+                preparedStatement.setNull(index++, java.sql.Types.INTEGER);
+            }
+            else {
+                preparedStatement.setInt(index++, mnNodeId_n);
+            }
+    
+            String mysqlDatabaseURL = connection.getMetaData().getURL();
+            
+            String databaseHost = SBaseXUtils.getDbHostFromUrl(mysqlDatabaseURL);
+            String databaseName = SBaseXUtils.getDbNameFromUrl(mysqlDatabaseURL);
+            
+            // Company BaseX database connection
+            SBaseXClient baseXSession = SBaseXUtils.getBaseXSessionInstance(databaseHost, 1984, "admin", "admin");
+              
+            // escape XML special characters.
+            String xmlDocBody = SBaseXUtils.escapeSpecialCharacters(msDocXml);
+              
+            // Parse the xml body and add it to the BaseX database.
+            String addXmlToDBQuery = "db:add(\"" + databaseName + "\", fn:parse-xml(\"" + xmlDocBody + "\"), \"" + msDocXmlName + "\")";
+                     
+            SBaseXUtils.executeBaseXQuery(baseXSession, addXmlToDBQuery);
+              
+            // Obtain the last inserted document of the BaseX database and return its node-id
+            String getLastDBEntryQuery
+                                = "let $last := collection(\"" + databaseName + "\")[last()]\n"
+                                + "return db:node-id($last)";
+            ArrayList<String> queryResult = SBaseXUtils.executeBaseXQuery(baseXSession, getLastDBEntryQuery);
+            
+            // Insert the document node-id and add it to the MySQL database as reference. 
+            String updateNodeId_nQuery = "UPDATE trn_cfd SET node_id_n = " + queryResult.get(0) + " WHERE id_cfd = " + mnPkCfdId + ";";
+            connection.createStatement().executeUpdate(updateNodeId_nQuery);              
+            
             preparedStatement.setString(index++, SLibUtils.textToSql(msDocXml));
             preparedStatement.setString(index++, SLibUtils.textToSql(msDocXmlName));
             preparedStatement.setString(index++, msDocXmlRfcEmi);
