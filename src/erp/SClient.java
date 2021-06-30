bb1cbcd85aa435671dfb6c1282f85e5dc8e34405
Isabel <Isabel@DESKTOP-E0IIEV9>
2021-06-30 10:06:24 -0500

    SIIE 3.2 186.0/ 2021-06-30/ Isabel Danae GARCÍA SERVÍN/ 1) Se agregó un nuevo permiso para restringir el timbrado de los documentos de ciertos asoc. de negocios. 2) Se agregó el campo de posiciones en la tabla de puestos. 3) Se agregó en el módulo de RH el reporte de posiciones y vacantes.  4) Se restringió la modificación de los campos de serie y folio en la forma de captura de DPS cuando estos son generados automáticamente, además se añadio la actualización de estos campos cuando ya existen la serie y el folio previamente asignados. 5) Se modificó el resultado del comando "lock" en el servidor para que se muestre la cantidad, el usuario y la sesion de los candados.

--- src/erp/SClient.java
+++ src/erp/SClient.java
@@ -107,7 +107,7 @@ import sa.lib.xml.SXmlUtils;
 public class SClient extends JFrame implements ActionListener, SClientInterface, SGuiClient {
 
     public static final String APP_NAME = "SIIE 3.2";
-    public static final String APP_RELEASE = "3.2 184.3"; // fecha release: 2021-06-29
+    public static final String APP_RELEASE = "3.2 186.0"; // fecha release: 2021-06-30
     public static final String APP_COPYRIGHT = "2007-2021";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
