b8c3b2413abe45bcc3e6742689f3a52c68cd6fb6
Sergio Flores <sflores@swaplicado.com.mx>
2022-02-09 14:58:36 -0600

    SIIE 3.2 195.4/ 2022-02-09/ Sergio Abraham FLORES GUTIÉRREZ/ 1) Mejoramiento a la rapidez del procesamiento de CFDI de recepción de pagos.

--- src/erp/SClient.java
+++ src/erp/SClient.java
@@ -110,7 +110,7 @@ import sa.lib.xml.SXmlUtils;
 public class SClient extends JFrame implements ActionListener, SClientInterface, SGuiClient {
 
     public static final String APP_NAME = "SIIE 3.2";
-    public static final String APP_RELEASE = "3.2 195.3"; // fecha release: 2022-02-02
+    public static final String APP_RELEASE = "3.2 195.4"; // fecha release: 2022-02-09
     public static final String APP_COPYRIGHT = "2007-2022";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
