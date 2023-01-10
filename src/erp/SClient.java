a18c0e7dd14c434edd049348ecf5b320cfd24795
Sergio Flores <sflores@swaplicado.com.mx>
2023-01-10 09:21:40 -0600

    Merge origin/development into development
    Conflicts:
    	src/erp/SClient.java
    	src/erp/maintenancelog.txt

--- src/erp/SClient.java
+++ src/erp/SClient.java
@@ -112,7 +112,6 @@ public class SClient extends JFrame implements ActionListener, SClientInterface,
 
     public static final String APP_NAME = "SIIE 3.2";
     public static final String APP_RELEASE = "3.2 209.1"; // fecha release: 2023-01-09
-    public static final String APP_RELEASE = "3.2 209.1"; // fecha release: 2023-01-05
     public static final String APP_COPYRIGHT = "2007-2023";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
