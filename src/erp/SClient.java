9eff29ff49396ed64342f71d904a257fb2875152
Sergio Flores <sflores@swaplicado.com.mx>
2023-12-19 08:54:57 -0600

    SIIE 3.2 231.0/ 2023-12-18/ Isabel Danae GARCÍA SERVÍN/ Sergio Abraham FLORES GUTIÉRREZ/ 1) Eliminación de la tabla presentaciones de materiales. 2) Ajustes al formato de impresión de facturas FSC para Cartró.

--- src/erp/SClient.java
+++ src/erp/SClient.java
@@ -114,8 +114,8 @@ import sa.lib.xml.SXmlUtils;
 public class SClient extends JFrame implements ActionListener, SClientInterface, SGuiClient {
 
     public static final String APP_NAME = "SIIE 3.2";
-    public static final String APP_RELEASE = "3.2 230.1"; // fecha release: 2023-12-15
-    public static final String APP_COPYRIGHT = "2007-2023";
+    public static final String APP_RELEASE = "3.2 231.0"; // fecha release: 2023-12-18
+    public static final String APP_COPYRIGHT = "2007-2024";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
     public static final String VENDOR = APP_PROVIDER;
