609f67f48a35227f622409d9fd154ece5fae2505
Sergio Flores <sflores@swaplicado.com.mx>
2024-02-28 10:41:03 -0600

    SIIE 3.2 240.3/ 2024-02-28/ Sergio Abraham FLORES GUTIÉRREZ/ 1) Corrección al formato de impresión de contratos para mostrar el RFC del "receptor", y que se adapte a la categoría del contrato: compras o ventas. 2) Adición de columnas correo-e y teléfono tanto personal como de la empresa, y extensión del conmutador a la vista de datos personales de empleados.

--- src/erp/SClient.java
+++ src/erp/SClient.java
@@ -108,13 +108,13 @@ import sa.lib.xml.SXmlUtils;
 /**
  * SIIE Client.
  * 
- * @author  Sergio Flores, Uriel Castañeda, Juan Barajas, Isabel Servín, Sergio Flores, Edwin Carmona
+ * @author  Sergio Flores, Uriel Castañeda, Juan Barajas, Isabel Servín, Edwin Carmona, Sergio Flores
  * @version 3.2
  */
 public class SClient extends JFrame implements ActionListener, SClientInterface, SGuiClient {
 
     public static final String APP_NAME = "SIIE 3.2";
-    public static final String APP_RELEASE = "3.2 240.2"; // fecha release: 2024-02-21
+    public static final String APP_RELEASE = "3.2 240.3"; // fecha release: 2024-02-28
     public static final String APP_COPYRIGHT = "2007-2024";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
