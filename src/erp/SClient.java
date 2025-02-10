a0608abf9a0f26f2e8a9fa159d26c28891dd15d9
Isabel Servín <isabel.garcia@swaplicado.com.mx>
2025-02-10 11:50:52 -0600

    SIIE 3.2 263.0/ 2025-02-10 Isabel Danae GARCÍA SERVÍN/ 1) Creación de la pantalla para redactar comentarios en formas de captura, por el momento sólo implementada en RM. 2) Se agrega validación para guardar la pantalla de archivos de soporte sólo cuando no haya un archivo pendiente por guardar. 3) Se agrega un sistema de prioridad al enviar ordenes de compra a autorización vía app web.

--- src/erp/SClient.java
+++ src/erp/SClient.java
@@ -114,7 +114,7 @@ import sa.lib.xml.SXmlUtils;
 public class SClient extends JFrame implements ActionListener, SClientInterface, SGuiClient {
 
     public static final String APP_NAME = "SIIE 3.2";
-    public static final String APP_RELEASE = "3.2 262.3"; // fecha release: 2025-02-06
+    public static final String APP_RELEASE = "3.2 263.0"; // fecha release: 2025-02-10
     public static final String APP_COPYRIGHT = "2007-2025";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
