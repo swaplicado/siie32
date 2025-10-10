2dfcd60b01c63ac030037a1035ca7d1077061746
Isabel Servín <isabel.garcia@swaplicado.com.mx>
2025-10-10 09:20:24 -0600

    SIIE 3.2 290.18/ 2025-10-10 Isabel Danae GARCÍA SERVÍN/ 1) Mostrar el el monto de pagos pendientes y el saldo insoluto en el picker de documentos para solicitudes de pago. 2) Elección del flujo de autorización al enviar a autorizar las solicitudes de pago. 3) Impedir la modificación de la selección de documentos y la cantidad en el grid de layouts bancarios cuando se capturan a partir de solicitudes de pago. 4) Permitir la modificación de OC cuando estas están rechazadas. 5) Adición de indicador visual en la vista de OC, amarillo cuando su estatus sea "En envío" o rojo cuando su estatus sea "Fallo de envío".

--- src/erp/SClient.java
+++ src/erp/SClient.java
@@ -126,7 +126,7 @@ import sa.lib.xml.SXmlUtils;
 public class SClient extends JFrame implements SClientInterface, SGuiClient, ActionListener {
 
     public static final String APP_NAME = "SIIE 3.2";
-    public static final String APP_RELEASE = "3.2 290.17"; // release date: 2025-10-09
+    public static final String APP_RELEASE = "3.2 290.18"; // release date: 2025-10-10
 
     public static final String APP_COPYRIGHT = "2007-2025";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
