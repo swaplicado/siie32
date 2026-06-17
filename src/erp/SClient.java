01f4b977cb15ab06cabeb44268d81e5fd71897c9
Sergio Flores <sflores@swaplicado.com.mx>
2026-06-17 11:29:05 -0500

    Release SIIE 3.2 302.5.

--- src/erp/SClient.java
+++ src/erp/SClient.java
@@ -127,7 +127,7 @@ import sa.lib.xml.SXmlUtils;
 public class SClient extends JFrame implements SClientInterface, SGuiClient, ActionListener {
 
     public static final String APP_NAME = "SIIE 3.2";
-    public static final String APP_RELEASE = "3.2 302.5"; // release date: 2026-06-15
+    public static final String APP_RELEASE = "3.2 302.5"; // release date: 2026-06-17
 
     public static final String APP_COPYRIGHT = "2007-2026";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
