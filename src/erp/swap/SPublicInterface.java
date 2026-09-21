c8d01f1e36e4c75fe8a5ac85a0338666db848004
Edwin Carmona <eedwincarmona@gmail.com>
2026-09-21 13:28:31 -0600

    app-link: Actualización de recurso desde sistema externo

--- src/erp/swap/SPublicInterface.java
+++ src/erp/swap/SPublicInterface.java
@@ -5,15 +5,17 @@
  */
 package erp.swap;
 
-import erp.swap.utils.SExportDataUser;
-import erp.swap.utils.SExportDataUtils;
-import erp.swap.utils.SResourceStatusResponse;
 import erp.mod.hrs.link.db.SConfigException;
 import erp.mod.hrs.link.db.SMySqlClass;
 import erp.mod.trn.api.db.STrnDBCore;
+import erp.swap.utils.SExportDataUser;
+import erp.swap.utils.SExportDataUtils;
+import erp.swap.utils.SResourceResponse;
+import erp.swap.utils.SSwapHttpUtils;
 import java.net.HttpURLConnection;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -91,7 +93,7 @@ public class SPublicInterface {
         return null;
     }
     
-    public SResourceStatusResponse updateResourceStatus(final int companyId, 
+    public SResourceResponse updateResourceStatus(final int companyId, 
                                                             final int resourceType, 
                                                             final String resourceId, 
                                                             final int authStatusId, 
@@ -100,11 +102,11 @@ public class SPublicInterface {
                                                             final double newAmount,
                                                             final String newDate) {
         try {
-            SResourceStatusResponse oResponse;
+            SResourceResponse oResponse;
             Connection conn = this.getConnection(companyId);
 
             if (conn == null) {
-                oResponse = new SResourceStatusResponse();
+                oResponse = new SResourceResponse();
                 oResponse.status_code = HttpURLConnection.HTTP_INTERNAL_ERROR;
                 oResponse.message = "No se pudo establecer conexión con la base de datos de SIIE.";
                 oResponse.error = "No se pudo establecer conexión con la base de datos de SIIE.";
@@ -113,7 +115,7 @@ public class SPublicInterface {
                 return oResponse;
             }
             
-            return SExportDataUtils.updateResourceStatus(conn.createStatement(), 
+            return SSwapHttpUtils.updateResourceStatus(conn.createStatement(), 
                                                         this.oDbObj.getMainDatabaseName(companyId), 
                                                         resourceType, 
                                                         resourceId, 
@@ -126,6 +128,63 @@ public class SPublicInterface {
         catch (SQLException ex) {
             Logger.getLogger(STrnDBCore.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SPublicInterface.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+
+    /**
+     * Actualiza un recurso específico en la base de datos.
+     *
+
+     * @param companyId El ID de la compañía.
+     * @param resourceType El tipo de recurso a actualizar.
+     * @param resourceId El ID del recurso a actualizar.
+     * @param userId El ID del usuario que realiza la actualización.
+     * @param lAttributes Un HashMap que contiene los atributos a actualizar.
+     * 
+     * @return Un objeto SResourceResponse que contiene la respuesta de la operación.
+     */
+    public SResourceResponse updateResource(final int companyId, 
+                                            final int resourceType, 
+                                            final String resourceId,
+                                            final int userId,
+                                            final HashMap<String, Object> lAttributes) {
+        try {
+            SResourceResponse oResponse;
+            if (lAttributes.isEmpty()) {
+                oResponse = new SResourceResponse();
+                oResponse.status_code = HttpURLConnection.HTTP_BAD_REQUEST;
+                oResponse.message = "El cuerpo de la solicitud está vacío.";
+                oResponse.error = "El cuerpo de la solicitud está vacío.";
+                oResponse.data = null;
+
+                return oResponse;
+            }
+            Connection conn = this.getConnection(companyId);
+
+            if (conn == null) {
+                oResponse = new SResourceResponse();
+                oResponse.status_code = HttpURLConnection.HTTP_INTERNAL_ERROR;
+                oResponse.message = "No se pudo establecer conexión con la base de datos de SIIE.";
+                oResponse.error = "No se pudo establecer conexión con la base de datos de SIIE.";
+                oResponse.data = null;
+                
+                return oResponse;
+            }
+            
+            return SSwapHttpUtils.updateResource(conn.createStatement(), 
+                                            this.oDbObj.getMainDatabaseName(companyId), 
+                                            resourceType, 
+                                            resourceId,
+                                            userId,
+                                            lAttributes);
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(STrnDBCore.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (ClassNotFoundException ex) {
             Logger.getLogger(SPublicInterface.class.getName()).log(Level.SEVERE, null, ex);
         }
 
--- src/erp/swap/utils/SExportDataUtils.java
+++ src/erp/swap/utils/SExportDataUtils.java
@@ -33,8 +33,6 @@ import erp.swap.SSwapConsts;
 import erp.swap.SSwapUtils;
 import erp.swap.SSyncType;
 import java.io.File;
-import java.net.HttpURLConnection;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -3110,130 +3108,6 @@ public abstract class SExportDataUtils {
         }
 
         return user;
-    }
-
-    /**
-     * Update status of authorizable resource.
-     *
-     * @param statement
-     * @param companyDbName
-     * @param resourceType
-     * @param resourceId
-     * @param authStatusId
-     * @param userId
-     * @param notes
-     * @param newAmount
-     * @param newDate
-     * @return
-     */
-    public static SResourceStatusResponse updateResourceStatus(final Statement statement, final String companyDbName, final int resourceType, final String resourceId, final int authStatusId,
-            final int userId, final String notes, final double newAmount, final String newDate) {
-        SResourceStatusResponse oResponse;
-
-        try {
-            oResponse = new SResourceStatusResponse();
-            String sTable = "";
-            String sWhere = "";
-            String sUpdate = "";
-            String sSecondQuery = "";
-
-            switch (resourceType) {
-                case SSwapConsts.RESOURCE_TYPE_PUR_PAYMENT:
-                    sTable = SModConsts.TablesMap.get(SModConsts.FIN_PAY);
-
-                    switch (authStatusId) {
-                        case SSwapConsts.AUTHZ_STATUS_OK:
-                            sUpdate = "fk_st_pay = " + SModSysConsts.FINS_ST_PAY_SCHED_P + ", "
-                                    + "fk_usr_sched = " + userId + ", "
-                                    + "ts_usr_sched = NOW(), ";
-                            if (newAmount > 0) {
-                                // en partida: des_pay_app_ety_cur
-                                sSecondQuery = "UPDATE " + companyDbName + "." + SModConsts.TablesMap.get(SModConsts.FIN_PAY_ETY) + " SET "
-                                        + "des_pay_app_ety_cur = " + newAmount + " "
-                                        + "WHERE (id_pay = " + resourceId + ") and (id_ety = 1);";
-                            }
-                            if (newDate != null && !newDate.isEmpty()) {
-                                sUpdate += "dt_sched_n = '" + newDate + "', ";
-                            }
-                            else {
-                                sUpdate += "dt_sched_n = dt_req, ";
-                            }
-                            break;
-
-                        case SSwapConsts.AUTHZ_STATUS_REJECTED:
-                            sUpdate = "fk_st_pay = " + SModSysConsts.FINS_ST_PAY_REJC_P + ", "
-                                    + "dt_sched_n = NULL, "
-                                    + "fk_usr_sched = " + SUtilConsts.USR_NA_ID + ", "
-                                    + "ts_usr_sched = NOW(), "; // XXX se puede actualizar este TS o no en el rechazo, por lo pronto se deja
-                            break;
-
-                        default:
-                            oResponse.status_code = HttpURLConnection.HTTP_BAD_REQUEST;
-                            oResponse.message = "El tipo de estatus de autorización es desconocido (" + authStatusId + ").";
-                            oResponse.error = "Tipo de estatus de autorización desconocido.";
-
-                            return oResponse;
-                    }
-
-                    sUpdate += "nts_auth_flow = '" + SLibUtils.textToSql(notes) + "', "
-                            + "fk_usr_upd = " + userId + ", "
-                            + "ts_usr_upd = NOW() ";
-
-                    sWhere = "WHERE id_pay = " + resourceId;
-                    break;
-
-                default:
-                    oResponse.status_code = HttpURLConnection.HTTP_BAD_REQUEST;
-                    oResponse.message = "No se encontró tipo de recurso.";
-                    oResponse.error = "No se encontró tipo de recurso.";
-
-                    return oResponse;
-            }
-
-            String sql = "UPDATE " + companyDbName + "." + sTable + " SET " + sUpdate + sWhere + ";";
-            Logger.getLogger(SExportDataUtils.class.getName()).log(Level.INFO, "ACTUALIZAR PAGO, company: {0}. QUERY: {1} ", new Object[]{companyDbName, sql});
-
-            // Iniciar transacción
-            Connection conn = statement.getConnection();
-            boolean autoCommit = conn.getAutoCommit();
-            try {
-                conn.setAutoCommit(false);
-
-                int res = statement.executeUpdate(sql);
-                if (sSecondQuery != null && !sSecondQuery.isEmpty()) {
-                    Logger.getLogger(SExportDataUtils.class.getName()).log(Level.INFO, "ACTUALIZAR PARTIDA PAGO, company: {0}. QUERY{1}: ", new Object[]{companyDbName, sSecondQuery});
-                    res = statement.getConnection().createStatement().executeUpdate(sSecondQuery);
-                }
-                if (res != 1) {
-                    conn.rollback();
-                    oResponse.status_code = HttpURLConnection.HTTP_INTERNAL_ERROR;
-                    oResponse.message = "No se realizó ninguna actualización.";
-                    oResponse.error = "No se realizó ninguna actualización.";
-                }
-                else {
-                    conn.commit();
-                    oResponse.status_code = HttpURLConnection.HTTP_OK;
-                    oResponse.message = "OK";
-                }
-            }
-            catch (SQLException ex) {
-                Logger.getLogger(SExportDataUtils.class.getName()).log(Level.SEVERE, null, ex);
-                conn.rollback();
-                throw ex;
-            }
-            finally {
-                conn.setAutoCommit(autoCommit);
-            }
-        }
-        catch (SQLException ex) {
-            oResponse = new SResourceStatusResponse();
-            oResponse.status_code = HttpURLConnection.HTTP_INTERNAL_ERROR;
-            oResponse.message = "Error al actualizar el estatus del recurso.";
-            oResponse.error = ex.getMessage();
-            Logger.getLogger(SExportDataUtils.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return oResponse;
     }
 
     public static class BankAccount {
